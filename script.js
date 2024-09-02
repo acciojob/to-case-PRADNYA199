48daf9448df365642c8a44812f855050b3d9198b
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-09-02 10:46:21 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function toCase(text) {
   // write your code here
+	if(text==='-')
+	{
+		return "-"
+	}
+		const lowercase=text.toLowerCase();
+		const uppercase=text.roUpperCase();
+	 return `${lowercase}-${uppercase}`;
+	 
 }
 
 // DO not change the code below
